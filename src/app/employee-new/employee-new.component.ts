e5e7f2c7a909706ffb608f1cfd3d15b6bb332b73
Rafael Barros <rafaelbarros.df@gmail.com>
2018-09-04 23:53:53 -0300

    Lidando com coleção de dados

--- src/app/employee-new/employee-new.component.ts
+++ src/app/employee-new/employee-new.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import employees from '../employees';
 
 @Component({
   selector: 'employee-new',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeeNewComponent implements OnInit {
 
-  name = 'Rafael';
+  name = '';
+  salary = 0;
 
-  employees = [];
+  employees = employees;
 
   constructor() { }
 
@@ -18,7 +20,7 @@ export class EmployeeNewComponent implements OnInit {
 
   addEmployee(event) {
     console.warn(event);
-    this.employees.push(this.name);
+    this.employees.push({name: this.name, salary: this.salary});
     console.log(this.employees);
   }
 
---
+++ src/app/employees.ts
@@ -0,0 +1,7 @@
+interface Employee {
+    name: string;
+    salary: number;
+}
+const employees: Employee[] = [];
+
+export default employees;
