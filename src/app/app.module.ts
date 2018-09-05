3e8c46f9adeacdf5afa1bf79c8fb8784bb222b94
Rafael Barros <rafaelbarros.df@gmail.com>
2018-09-04 23:27:58 -0300

    Iniciando com Data binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { EmployeeNewComponent } from './employee-new/employee-new.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmployeeListComponent
+    EmployeeListComponent,
+    EmployeeNewComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/employee-new/employee-new.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'employee-new',
+  templateUrl: './employee-new.component.html',
+  styleUrls: ['./employee-new.component.css']
+})
+export class EmployeeNewComponent implements OnInit {
+
+  name = 'Rafael';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
