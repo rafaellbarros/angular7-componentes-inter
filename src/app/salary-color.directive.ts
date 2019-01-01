10e406c899a97e06b1ce7d8da4b6fe261070bfe8
Rafael Barros <rafaelbarros.df@gmail.com>
2019-01-01 16:07:50 -0200

    Melhoria na diretiva salaryColor

--- src/app/salary-color.directive.ts
+++ src/app/salary-color.directive.ts
@@ -5,17 +5,14 @@ import { Directive, ElementRef, Input } from '@angular/core';
 })
 export class SalaryColorDirective {
 
-  @Input()
-  salaryColor;
-
   constructor(private element: ElementRef) {
-    setTimeout(() => {
-      const nativeElement: HTMLElement = this.element.nativeElement;
-      const salary = parseFloat(this.salaryColor);
-      nativeElement.style.color = salary > 20000 ? 'green' : 'red';
-      // this.element.nativeElement.innerHTML = this.salaryColor;
-    }, 2000);
+  }
 
+  @Input()
+  set salaryColor(value) { // setter
+    const nativeElement: HTMLElement = this.element.nativeElement;
+    const salary = parseFloat(value);
+    nativeElement.style.color = salary > 20000 ? 'green' : 'red';
   }
 
 }
